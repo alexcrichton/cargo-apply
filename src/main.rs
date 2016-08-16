0165dbd1215759ab79077dc899b13ede61db3d14
Niko Matsakis <niko@alum.mit.edu>
2016-08-16 18:51:38 -0400

    add docopt and command-line flags

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate cargo;
+extern crate docopt;
 extern crate git2;
 extern crate walkdir;
 extern crate libc;
+extern crate regex;
+extern crate rustc_serialize;
 
 use std::env;
+use std::fmt;
 use std::fs::{self, File};
 use std::io::prelude::*;
 use std::io;
@@ -14,10 +18,47 @@ use cargo::ops;
 use cargo::sources::RegistrySource;
 use cargo::core::shell::{Shell, MultiShell, Verbosity, ShellConfig, ColorConfig};
 use cargo::util::Config;
+use docopt::Docopt;
+use regex::Regex;
 use walkdir::{WalkDir, DirEntry, WalkDirIterator};
 
+// Write the Docopt usage string.
+const USAGE: &'static str = r#"
+Usage: cargo-apply [options] <package-name>...
+       cargo-apply --help
+
+Builds or tests the latest version of packages from crates.io, saving
+timing information and other results. If the special package-name "*"
+is used, we will test all packages. (Use `'*'` to prevent your shell
+from expanding wildcards.)
+
+WARNING: Building or testing packages from crates.io involves executing
+arbitary code! Be wary.
+
+Options:
+    --out DIR              Output directory [default: work].
+    --release              Use release mode instead of debug.
+"#;
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    flag_out: String,
+    flag_release: bool,
+    arg_package_name: Vec<String>,
+}
+
+#[derive(Clone, Debug)]
+struct KrateName {
+    name: String,
+    version: Option<String>,
+}
+
 fn main() {
-    let ref work_dir = PathBuf::from("work");
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.argv(env::args()).decode())
+        .unwrap_or_else(|e| e.exit());
+
+    let ref work_dir = PathBuf::from(&args.flag_out);
     let ref index_path = work_dir.join("index");
     let ref tmp_index_path = work_dir.join(".index");
     let ref cargo_dir = work_dir.join(".cargo");
@@ -31,7 +72,8 @@ fn main() {
     if fs::metadata(index_path).is_err() {
         println!("initializing registry index");
         git2::Repository::clone("https://github.com/rust-lang/crates.io-index",
-                                tmp_index_path).unwrap();
+                                tmp_index_path)
+            .unwrap();
         fs::rename(tmp_index_path, index_path).unwrap();
     }
 
@@ -46,18 +88,20 @@ fn main() {
         target-dir = './target'
     ");
 
-    File::create(cargo_dir.join("config")).unwrap()
-        .write_all(cargo_config.as_bytes()).unwrap();
+    File::create(cargo_dir.join("config"))
+        .unwrap()
+        .write_all(cargo_config.as_bytes())
+        .unwrap();
 
-    let crates = WalkDir::new(index_path).into_iter()
-        .filter_entry(|e| !bad(e))
-        .filter_map(|e| e.ok())
-        .filter(|e| e.file_type().is_file())
-        .map(|e| e.file_name().to_str().unwrap().to_string());
+    let crates: Vec<_> = match assemble_crate_names(&args, &index_path) {
+        Ok(v) => v,
+        Err(()) => return,
+    };
 
     for krate in crates {
-        let ref crate_stdio_dir = stdio_dir.join(&krate);
-        let ref crate_result_dir = results_dir.join(&krate);
+        let krate_str = krate.to_string();
+        let ref crate_stdio_dir = stdio_dir.join(&krate_str);
+        let ref crate_result_dir = results_dir.join(&krate_str);
         let ref result_file = crate_result_dir.join("results.txt");
 
         fs::create_dir_all(crate_stdio_dir).unwrap();
@@ -70,7 +114,7 @@ fn main() {
 
         println!("working on: {}", krate);
 
-        let result = build(work_dir, crate_stdio_dir, &krate);
+        let result = build(work_dir, crate_stdio_dir, &krate_str, &args);
         report_result(&result_file, result);
     }
 }
@@ -91,9 +135,9 @@ fn report_result(result_file: &Path, r: BuildResult) {
 
 fn bad(entry: &DirEntry) -> bool {
     entry.file_name()
-         .to_str()
-         .map(|s| s.starts_with(".") || s.ends_with(".json"))
-         .unwrap_or(false)
+        .to_str()
+        .map(|s| s.starts_with(".") || s.ends_with(".json"))
+        .unwrap_or(false)
 }
 
 fn config(work_dir: &Path, out: Box<Write + Send>, err: Box<Write + Send>) -> Config {
@@ -106,7 +150,8 @@ fn config(work_dir: &Path, out: Box<Write + Send>, err: Box<Write + Send>) -> Co
     let err = Shell::create(Box::new(err), config);
     Config::new(MultiShell::new(out, err, Verbosity::Verbose),
                 work_dir.to_owned(),
-                work_dir.join("cargo-home")).unwrap()
+                work_dir.join("cargo-home"))
+        .unwrap()
 }
 
 enum BuildResult {
@@ -116,13 +161,11 @@ enum BuildResult {
     Panic(String),
 }
 
-fn build(work_dir: &Path, out_dir: &Path, krate: &str) -> BuildResult {
+fn build(work_dir: &Path, out_dir: &Path, krate: &str, args: &Args) -> BuildResult {
 
     use std::panic::catch_unwind;
 
-    let r = catch_unwind(|| {
-        build_(work_dir, out_dir, krate)
-    });
+    let r = catch_unwind(|| build_(work_dir, out_dir, krate, args));
 
     match r {
         Ok(r) => r,
@@ -136,7 +179,7 @@ fn build(work_dir: &Path, out_dir: &Path, krate: &str) -> BuildResult {
     }
 }
 
-fn build_(work_dir: &Path, stdio_dir: &Path, krate: &str) -> BuildResult {
+fn build_(work_dir: &Path, stdio_dir: &Path, krate: &str, args: &Args) -> BuildResult {
 
     let out = File::create(stdio_dir.join("stdout")).unwrap();
     let err = File::create(stdio_dir.join("stderr")).unwrap();
@@ -145,8 +188,12 @@ fn build_(work_dir: &Path, stdio_dir: &Path, krate: &str) -> BuildResult {
     let mut src = RegistrySource::new(&id, &config);
 
     let dep = Dependency::parse(krate, None, &id).unwrap();
-    let pkg = src.query(&dep).unwrap().iter().map(|v| v.package_id())
-                 .max().cloned();
+    let pkg = src.query(&dep)
+        .unwrap()
+        .iter()
+        .map(|v| v.package_id())
+        .max()
+        .cloned();
     let pkg = match pkg {
         Some(pkg) => pkg,
         None => {
@@ -161,8 +208,8 @@ fn build_(work_dir: &Path, stdio_dir: &Path, krate: &str) -> BuildResult {
         }
     };
 
-    let args = &["-Z".to_string(), "time-passes".to_string()];
-    let opts = compiler_opts(&config, args);
+    let rustc_args = &["-Z".to_string(), "time-passes".to_string()];
+    let opts = compiler_opts(&config, rustc_args, args);
     let res = ops::compile_pkg(&pkg, None, &opts);
 
     if let Err(e) = res {
@@ -170,7 +217,7 @@ fn build_(work_dir: &Path, stdio_dir: &Path, krate: &str) -> BuildResult {
     }
 
     let opts = &ops::TestOptions {
-        compile_opts: compiler_opts(&config, &[]),
+        compile_opts: compiler_opts(&config, &[], &args),
         no_run: false,
         no_fail_fast: false,
     };
@@ -184,7 +231,7 @@ fn build_(work_dir: &Path, stdio_dir: &Path, krate: &str) -> BuildResult {
     BuildResult::Success
 }
 
-fn compiler_opts<'a>(config: &'a Config, args: &'a [String]) -> ops::CompileOptions<'a> {
+fn compiler_opts<'a>(config: &'a Config, rustc_args: &'a [String], args: &Args) -> ops::CompileOptions<'a> {
     ops::CompileOptions {
         config: config,
         jobs: None,
@@ -200,9 +247,58 @@ fn compiler_opts<'a>(config: &'a Config, args: &'a [String]) -> ops::CompileOpti
             benches: &[],
         },
         exec_engine: None,
-        release: true,
+        release: args.flag_release,
         mode: ops::CompileMode::Build,
-        target_rustc_args: Some(args),
+        target_rustc_args: Some(rustc_args),
         target_rustdoc_args: None,
+    }
+}
+
+fn assemble_crate_names(args: &Args, index_path: &Path) -> Result<Vec<KrateName>, ()> {
+    if args.arg_package_name.iter().any(|s| s == "*") {
+        // assemble the list from the index
+        Ok(WalkDir::new(index_path)
+            .into_iter()
+            .filter_entry(|e| !bad(e))
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file())
+            .map(|e| e.file_name().to_str().unwrap().to_string())
+            .map(|f| {
+                KrateName {
+                    name: f,
+                    version: None,
+                }
+            })
+            .collect())
+    } else {
+        let regex = Regex::new(r"\s*([^=\s]+)\s*(?:=\s*[^=\s]+)?").unwrap();
+        args.arg_package_name
+            .iter()
+            .map(|str| {
+                match regex.captures(&str) {
+                    Some(captures) => {
+                        Ok(KrateName {
+                            name: captures.at(1).unwrap().to_string(),
+                            version: captures.at(2).map(|s| s.to_string()),
+                        })
+                    }
+                    None => {
+                        println!("invalid package name / version `{}`, try `foo` or `foo=0.1`",
+                                 str);
+                        Err(())
+                    }
+                }
+            })
+            .collect()
+    }
+}
+
+impl fmt::Display for KrateName {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        if let Some(ref ver) = self.version {
+            write!(fmt, "{}={}", self.name, ver)
+        } else {
+            write!(fmt, "{}", self.name)
+        }
     }
 }
